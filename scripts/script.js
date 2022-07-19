1761802e16dabb867da819b3eb1ecc91b383f28c
Anatoly Shuvakhin <shvkn@shvkn-mbp.local>
2022-07-19 16:48:37 +0700

    feature: Сверстать плавающий блок Оглавление

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,30 @@
-const toggleMenuBtn = document.querySelector('.nav-menu__toggle');
-const dropDownMenu = document.querySelector('.nav-menu__drop-container');
+const page = document.querySelector('.page');
+const header = page.querySelector('.header');
+const headerNavMenu = header.querySelector('.nav-menu');
+const toggleHeaderMenuButton = headerNavMenu.querySelector('.nav-menu__toggle');
+const headerToggleMenu = headerNavMenu.querySelector('.nav-menu__drop-container');
 
-const toggleMenu = (e) => {
-  dropDownMenu.classList.toggle('nav-menu__drop-container_expanded');
-  toggleMenuBtn.classList.toggle('nav-menu__toggle_expanded');
+const togglePreventPageScroll = () => {
+  page.classList.toggle('no-scroll');
 }
 
-toggleMenuBtn.addEventListener('click', toggleMenu);
+const toggleMenu = (e) => {
+  togglePreventPageScroll();
+  headerToggleMenu.classList.toggle('nav-menu__drop-container_expanded');
+  toggleHeaderMenuButton.classList.toggle('nav-menu__toggle_expanded');
+}
+toggleHeaderMenuButton.addEventListener('click', toggleMenu);
+
+
+const tocWidget = page.querySelector('.toc');
+const tocContent = tocWidget.querySelector('.toc__nav');
+const tocOpenButton = tocWidget.querySelector('.toc__open-button');
+const tocCloseButton = tocWidget.querySelector('.toc__close-button');
+
+const toggleTableOfContents = () => {
+  togglePreventPageScroll();
+  tocContent.classList.toggle('toc__nav_expanded');
+}
+
+tocOpenButton.addEventListener('click', toggleTableOfContents);
+tocCloseButton.addEventListener('click', toggleTableOfContents);
